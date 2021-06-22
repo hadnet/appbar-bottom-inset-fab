245ffbfe14bbbe9739f3f1fe522394e666c78b03
hadnet <hadnet1@gmail.com>
2021-06-22 20:04:37 -0300

    chore: initial commit

---
+++ src/AppbarBottom.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import {View, Dimensions, StyleSheet, SafeAreaView} from 'react-native';
+import Svg, {Path} from 'react-native-svg';
+import * as shape from 'd3-shape';
+import {BottomTabBarProps} from '@react-navigation/bottom-tabs';
+import Animated, {useAnimatedStyle, useSharedValue} from 'react-native-reanimated';
+import StaticBar, {TAB_HEIGHT as HEIGHT} from './StaticBar';
+
+export type AppbarBottomColorProps = {
+  bar: string;
+  fab: string;
+  icon: string;
+  activeIcon?: string;
+};
+
+export type AppbarBottomProps = {
+  color: AppbarBottomColorProps;
+};
+
+const {width} = Dimensions.get('window');
+
+const CURVE_WIDTH = 120;
+
+const left = (width: number) =>
+  shape
+    .line()
+    .x(([x]) => x)
+    .y(([_x, y]) => y)([
+    [0, 0],
+    [width, 0],
+  ]);
+
+const tab = (width: number, tabWidth: number) =>
+  shape
+    .line()
+    .x(([x]) => x)
+    .y(([_, y]) => y)
+    .curve(shape.curveBasis)([
+    [width + 23, 0],
+    [width + 25, 2],
+    [width + tabWidth / 4.33, 16],
+    [width + tabWidth / 3, 30],
+    [width + tabWidth / 2, 36.33],
+    [width + tabWidth - tabWidth / 3, 30],
+    [width + tabWidth - tabWidth / 4.33, 16],
+    [width + tabWidth - 25, 2],
+    [width + tabWidth - 23, 0],
+  ]);
+
+const right = (width: number, tabWidth: number) =>
+  shape
+    .line()
+    .x(([x]) => x)
+    .y(([_, y]) => y)([
+    [width + tabWidth, 0],
+    [width * 2.5, 0],
+    [width * 2.5, HEIGHT],
+    [0, HEIGHT],
+    [0, 0],
+  ]);
+
+const AnimatedSvg = Animated.createAnimatedComponent(Svg);
+
+export default function AppbarBottom(props: BottomTabBarProps & AppbarBottomProps) {
+  const {
+    state,
+    color: {bar},
+  } = props;
+  const tabFraction = width / state.routes.length;
+  const translateX = useSharedValue<number>(
+    width / (state.routes.length === 5 ? 5 : width) - width + width / state.routes.length,
+  );
+  const translateXAnimatedStyle = useAnimatedStyle(() => {
+    return {transform: [{translateX: translateX.value}]};
+  });
+  const d = `${left(width)} ${tab(width - (CURVE_WIDTH - tabFraction) / 2, CURVE_WIDTH)} ${right(
+    width,
+    width,
+  )}`;
+  return (
+    <>
+      <View>
+        <View {...{width, height: HEIGHT}} style={{position: 'absolute', bottom: 0}}>
+          <AnimatedSvg width={width * 2.5} {...{height: HEIGHT}} style={translateXAnimatedStyle}>
+            <Path {...{d}} fill={bar} />
+          </AnimatedSvg>
+          <View style={[StyleSheet.absoluteFill]}>
+            <StaticBar {...props} staticBarTranslateX={translateX} />
+          </View>
+        </View>
+      </View>
+      <SafeAreaView style={{backgroundColor: bar}} />
+    </>
+  );
+}
---
+++ src/StaticBar.tsx
@@ -0,0 +1,211 @@
+import React, {useEffect} from 'react';
+import {View, TouchableWithoutFeedback, StyleSheet, Dimensions} from 'react-native';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
+import {MCIconsNames} from './MaterialCommunityIcons.type';
+import {BottomTabBarProps} from '@react-navigation/bottom-tabs';
+import Animated, {
+  Easing,
+  Extrapolate,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
+import type {AppbarBottomProps} from './AppbarBottom';
+
+interface StaticBarProps extends BottomTabBarProps {
+  staticBarTranslateX: Animated.SharedValue<number>;
+}
+
+export const TAB_HEIGHT = 56;
+const {width} = Dimensions.get('window');
+
+export default function StaticBar(props: StaticBarProps & AppbarBottomProps) {
+  const {
+    /*descriptors, */ state,
+    navigation,
+    staticBarTranslateX,
+    color: {fab, activeIcon, icon},
+  } = props;
+  const {routes} = props.state;
+  const tabWidth = width / routes.length;
+  let circleBtns: Animated.SharedValue<number>[] = [];
+
+  const middleTab = (routes: number) => {
+    return routes === 3 ? 1 : routes === 5 ? 2 : null;
+  };
+
+  useEffect(() => {
+    const idx = middleTab(routes.length);
+    routes.map((route, index) => {
+      index === idx && navigation.navigate(route.name);
+    });
+  }, []);
+
+  const idx = middleTab(routes.length);
+  if (!idx) throw new Error('Bottom tabs must be of 3 tabs or 5 tabs');
+
+  circleBtns = routes.map((_, index) => useSharedValue(index === idx ? 1 : 0));
+
+  const start = (index: number) => {
+    circleBtns.map(circleBtn => {
+      circleBtn.value = withTiming(0, {
+        duration: 1,
+      });
+    });
+    circleBtns[index].value = withSpring(1, {
+      stiffness: 300,
+      damping: 100,
+      overshootClamping: true,
+    });
+    staticBarTranslateX.value = withTiming(-width + tabWidth * index);
+  };
+
+  return (
+    <View style={styles.container}>
+      {routes.map((route, key) => {
+        /*const {options} = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;*/
+
+        const isFocused = state.index === key;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+            start(key);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        const circleBtnActive = circleBtns[key];
+
+        const opacityAnimatedStyle = useAnimatedStyle(() => {
+          return {
+            opacity: withTiming(
+              interpolate(
+                staticBarTranslateX.value,
+                [
+                  -width + tabWidth * (key - 1),
+                  -width + tabWidth * key,
+                  -width + tabWidth * (key + 1),
+                ],
+                [1, 0, 1],
+                Extrapolate.CLAMP,
+              ),
+              {
+                duration: 0.01,
+                easing: Easing.inOut(Easing.ease),
+              },
+            ),
+          };
+        });
+
+        const translateYAnimatedStyle = useAnimatedStyle(() => {
+          return {
+            transform: [
+              {
+                translateY: withSpring(
+                  interpolate(circleBtnActive.value, [0, 1], [TAB_HEIGHT, -34]),
+                  {
+                    stiffness: 300,
+                    damping: 10,
+                    mass: 0.1,
+                    overshootClamping: true,
+                  },
+                ),
+              },
+            ],
+          };
+        });
+
+        return (
+          <React.Fragment key={key}>
+            <TouchableWithoutFeedback {...{key}} onPress={onPress} onLongPress={onLongPress}>
+              <Animated.View style={[styles.tab, opacityAnimatedStyle]}>
+                <MaterialCommunityIcons size={24} name={route.name as MCIconsNames} color={icon} />
+              </Animated.View>
+            </TouchableWithoutFeedback>
+            <Animated.View
+              style={[
+                {
+                  ...styles.circleWrapper,
+                  left: tabWidth * key,
+                  width: tabWidth,
+                },
+                translateYAnimatedStyle,
+              ]}
+            >
+              <View
+                style={[
+                  styles.circle,
+                  {
+                    backgroundColor: fab,
+                  },
+                ]}
+              >
+                <MaterialCommunityIcons
+                  size={24}
+                  name={route.name as MCIconsNames}
+                  color={activeIcon ?? icon}
+                />
+              </View>
+            </Animated.View>
+          </React.Fragment>
+        );
+      })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  tab: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: TAB_HEIGHT,
+  },
+  circleWrapper: {
+    top: 6,
+    height: TAB_HEIGHT,
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  circle: {
+    width: 56,
+    height: 56,
+    bottom: 0,
+    borderRadius: 28,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.39,
+    shadowRadius: 8.3,
+    elevation: 13,
+  },
+});
